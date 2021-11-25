994e62943a4acf309d2ea9c62a82429becde3b54
pr0fr3ss0r <84141032+pr0fr3ss0r@users.noreply.github.com>
2021-11-24 19:46:59 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 
 song = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 function preload()
 {
@@ -13,6 +17,27 @@ function setup() {
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + " leftWristY = " + leftWristY);
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + " rightWristY = " + rightWristY);
+    }
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Working Good And Correctly!!')
 }
 
 function draw() {
@@ -22,4 +47,6 @@ function draw() {
 function play()
 {
     song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
